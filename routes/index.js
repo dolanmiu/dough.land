7434f1eb393081e816e3b24ed0db91c6e3b7bf76
Dolan Miu <dolan_miu@hotmail.com>
2014-10-15 19:20:18 +0100

    Utilised mongodb in index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,59 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('index', { 
+            title: 'Express',
+            "messages" : docs
+        });
+    });
+  
+});
+
+router.get('/messages', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('messagelist', {
+            "messages" : docs
+        });
+    });
+});
+
+router.post('/addMessage', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    var xPos = req.body.xPos;
+    var zPos = req.body.yPos;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail,
+        "xPos" : xPos,
+        "zPos" : zPos
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("/");
+            // And forward to success page
+            res.redirect("/");
+        }
+    });
 });
 
 module.exports = router;
