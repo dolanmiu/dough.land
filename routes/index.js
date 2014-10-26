56202b6ee88c8315e3d50863260411544a1d6d78
Dolan Miu <dolan_miu@hotmail.com>
2014-10-26 03:26:31 +0000

    simplified index rendering

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    var db = req.db;
-    var collection = db.get('messagecollection');
-    collection.find({}, {}, function (e, docs) {
-        res.render('index', {
-            title: 'Express',
-            "messages": docs
-        });
+    res.render('index', {
+        title: 'Express'
     });
 
 });
